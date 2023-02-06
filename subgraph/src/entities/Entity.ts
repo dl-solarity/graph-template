0cf6a78ee727248020031f1e18f46d938629bc61
Volodymyr <zolotukhinvolodymyr@gmail.com>
2023-02-06 17:23:58 +0200

    updated template

--- subgraph/src/entities/Entity.ts
+++ subgraph/src/entities/Entity.ts
@@ -1,9 +1,31 @@
+import { BigInt, Bytes, Entity } from "@graphprotocol/graph-ts";
+
 export function getEntity(id: Bytes, optionalArg: BigInt = BigInt.zero()): Entity {
-  let entity = Entity.load(id);
-
-  if (entity == null) {
-    entity = new Entity(id);
+  if (isExistEntity(id)) {
+    const entity = Entity.load(id);
+    return entity;
+  } else {
+    throw new Error("Entity doesn't exist");
   }
+}
 
-  return entity;
+export function createEntity(id: Bytes, arg: BigInt): Entity {
+  if (!isExistEntity(id)) {
+    entity = new Entity(id);
+    return entity;
+  } else {
+    throw new Error("Entity already exists");
+  }
+}
+
+export function getOrCreateEntity(id: Bytes, optionalArg: BigInt = BigInt.zero()): Entity {
+  if (isExistEntity(id)) {
+    return Entity.load(entity);
+  } else {
+    return createEntity(id, optionalArg);
+  }
+}
+
+export function isExistEntity(id: Bytes): bool {
+  return Entity.load(id) != null;
 }
