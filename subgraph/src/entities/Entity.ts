46a968f9c6fc7691ddefabb395c33cb9e254f3ed
VolodymyrZolotukhin <60932900+VolodymyrZolotukhin@users.noreply.github.com>
2023-04-26 12:39:14 +0300

    updated template (#5)
    * updated template
    
    * renamed to entityExists

--- subgraph/src/entities/Entity.ts
+++ subgraph/src/entities/Entity.ts
@@ -1,9 +1,31 @@
+import { BigInt, Bytes, Entity } from "@graphprotocol/graph-ts";
+
 export function getEntity(id: Bytes, optionalArg: BigInt = BigInt.zero()): Entity {
-  let entity = Entity.load(id);
-
-  if (entity == null) {
-    entity = new Entity(id);
+  if (entityExists(id)) {
+    const entity = Entity.load(id);
+    return entity;
+  } else {
+    throw new Error("Entity doesn't exist");
   }
+}
 
-  return entity;
+export function createEntity(id: Bytes, arg: BigInt): Entity {
+  if (!entityExists(id)) {
+    entity = new Entity(id);
+    return entity;
+  } else {
+    throw new Error("Entity already exists");
+  }
+}
+
+export function getOrCreateEntity(id: Bytes, optionalArg: BigInt = BigInt.zero()): Entity {
+  if (entityExists(id)) {
+    return Entity.load(entity);
+  } else {
+    return createEntity(id, optionalArg);
+  }
+}
+
+export function entityExists(id: Bytes): bool {
+  return Entity.load(id) != null;
 }
